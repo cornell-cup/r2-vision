86e1bd59c27965bcb9235eba546b43e6284459ae
Michael Yuan <mty6@cornell.edu>
2018-03-06 17:43:13 -0500

    First version of R2-Vision

---
+++ find_location.cc
@@ -0,0 +1,20 @@
+#include <apriltag/apriltag.h>
+#include <apriltag/tag36h11.h>
+#include <apriltag/tag36artoolkit.h>
+#include <curl/curl.h>
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <unordered_map>
+
+using namespace cv;
+using std::vector;
+
+#define TAG_SIZE 6.5f
+
+int main(){
+	
+}
